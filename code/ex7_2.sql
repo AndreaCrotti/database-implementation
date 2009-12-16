7ee099c10a0649fba2666a0aa4651eff2e0a3477
Andrea Crotti <kerny404@gmail.com>
2009-12-16 12:06:15 +0100

    finalizing sql code

--- code/ex7_2.sql
+++ code/ex7_2.sql
@@ -5,13 +5,19 @@ CREATE TABLE boss(person_name PRIMARY KEY, manager_name);
 
 insert into lives values(1, 'mantova', 'pippo');
 insert into lives values(2, 'brescia', 'pippo');
-insert into lives values(3, 'topolina', 'abc');
+insert into lives values(3, 'brescia', 'abc');
+insert into lives values(4, 'Hamburg', 'abc');
+insert into lives values(5, 'nonwork', 'df');
 
 insert into works values(1, 'MyComp', 100);
 insert into works values(2, 'BigComp', 200);
+insert into works values(3, 'MyComp', 100000);
+insert into works values(4, 'Ham', 200);
 
 insert into located values('MyComp', 'brescia');
 insert into located values('BigComp', 'verona');
+insert into located values('Ham', 'Hamburg');
 
-insert into boss values(1, 'capo');
-insert into boss values(2, 'altrocapo');
+insert into boss values(1, 'firstboss');
+insert into boss values(2, 'secondboss');
+insert into boss values(4, 'third');
