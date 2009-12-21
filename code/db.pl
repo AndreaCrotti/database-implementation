e69eebb32001c1d51a59308b1f38727ed117da69
Andrea Crotti <kerny404@gmail.com>
2009-12-21 16:03:48 +0100

    sol9 pdf

--- code/db.pl
+++ code/db.pl
@@ -1,13 +1,25 @@
 factory('bigcomp').
 factory('bigcomp2').
-empl(1, 'milner', 'bigcomp').
-empl(2, 'adam', 'bigcomp2').
+
+city('guidizzolo').
+city('paperopoli').
+
+empl(1, 'milner', 'bigcomp', 100, 'guidizzolo').
+empl(2, 'adam', 'bigcomp2', 400, 'paperopoli').
+
+manager(1, 'paperopoli').
+
+boss(1, 1).
+boss(2, 3).
 
 % putting one after the other we get automatically and OR
 dep(X,Y) :-
 	factory(Y),
-	empl(_,X,Y).
+	empl(_,X,Y,_,_).
 
 dep(X,Y) :-
 	factory(Y),
-	empl(X,_,Y).
+	empl(X,_,Y,_,_).
+
+rich(X) :-
+	empl(X,_,_,S,_), S > 100.
