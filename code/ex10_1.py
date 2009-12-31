11e6d2cf93529ea4e299e2eaa467d76918ab6356
Andrea Crotti <kerny404@gmail.com>
2009-12-31 16:50:52 +0100

    start ex1

--- code/ex10_1.py
+++ code/ex10_1.py
@@ -6,14 +6,14 @@
 
 import sqlite3
 import random
-from itertools import izip
 
-db = "ex10_1.db"
+db_file = "ex10_1.db"
 # otherwise I can also use the memory
+cursor = sqlite3.connect(db_file)
 
 db_tables = {
-    "supplier" : ["id", "place.id", "name"],
     "place" : ["id", "district", "city", "country"],
+    "supplier" : ["id", "place.id", "name"],
     "employee" : [ "id", "place.id" , "nationality", "sex", "age" ],
     "gift" : [ "id" , "supplier.id", "name" , "price", "description"],
     "preference" : [ "gift.id" , "employee.id" , "note" ],
@@ -24,11 +24,13 @@ cardinality = {
     "gift" : 6000,
     "employee" : 10000,
     "supplier" : 500,
-    "place" : 1000
+    "place" : 1000,
+    "nationalities" : 50
     }
 
 def gen_db(tables):
-    FMT = "CREATE TABLE %s(%s)"
+    FMT = "CREATE TABLE %s(%s);"
+    tabs = []
     for t in tables:
         fields = []
         local = False
@@ -36,19 +38,47 @@ def gen_db(tables):
             if f == "id":
                 local = True
                 fields.append(" ".join([f, "PRIMARY KEY"]))
-            if f.find(".") > 0:
+            elif f.find(".") > 0:
                 # the use the foreign key thing
-                pass
+                fields.append(" ".join([f.split(".")[0], "FOREIGN KEY REFERENCES %s" % f]))
+            else:
+                fields.append(f)
         if not local:
+            # then the key is a combinantion of the references
             pass
-        
+        tabs.append(FMT % (t, ", ".join(fields)))
+    return tabs
+
 # Grammar for indexes: "CREATE INDEX <name> on <TABLE.field>"
 # this will be useful later in the query plan to optimize as much as possible
 
-def generate(item):
-    return ("item" + "_" + str(x) for x in xrange(cardinality[item]))
+def generate(item, card):
+    return (item + "_" + str(x) for x in xrange(card))
 
-# izip together the value to generate the new database
+
+sex = lambda: random.choice(('m', 'f'))
+age = lambda: random.choice(range(18,40))
+# is this enough for uniformly distributed stuff?
+price = lambda: random.choice(range(11, 210))
+
+nationalities = list(generate("nat", cardinality["nationalities"]))
+# there are 50 different nationalities
+nationality = lambda: random.choice(nationalities)
+# then every employee gets a shipment from where he comes from
+
+# in plus all the cities have same number of employees and more
+
+
+# query that need optimization
+query = """
+   SELECT gift.name, supplier.name, COUNT(*)
+   FROM gift, supplier, shipment
+   WHERE shipment.gift = gift.id AND shipment.supplier = supplier.id
+   AND gift.price > 190 AND supplier.place IN
+   (SELECT DISTINCT place.id
+   FROM employee, place
+   WHERE employee.place = place.id AND employee.nationality = 'German' )
+   GROUP BY gift.name, supplier.name
+"""
 
 sqltable = gen_db(db_tables.keys())
-
